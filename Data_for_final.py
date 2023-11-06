d966e2764ad4b93e0475b3840a5b4a3767cb8918
MadSchuman <144403496+MadSchuman@users.noreply.github.com>
2023-11-06 11:44:07 -0700

    Update Data_for_final.py

--- Data_for_final.py
+++ Data_for_final.py
@@ -11,7 +11,7 @@ from matplotlib import pyplot as plt
 pathname = "/Users/emmetshuman/Documents/CC Sophomore Folder/AppliedPY/Final project folder/GlobalLandTemperaturesByMajorCity.csv"
 
 def example1plot():
-  df = pd.read_csv(pathname, skiprows =1)
+  df = pd.read_csv(pathname)
   print(df.columns)
 
 
