59bea137fcd2fb950d5381d52e006141465ec514
rlwillner <122640077+rlwillner@users.noreply.github.com>
2023-11-06 13:51:47 -0500

    Update Data_for_final.py

--- Data_for_final.py
+++ Data_for_final.py
@@ -8,7 +8,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+pathname2 = '/Users/rebecca/Documents/code/appliedpython/project/GlobalLandTemperaturesByMajorCity.csv'
 pathname = "/Users/emmetshuman/Documents/CC Sophomore Folder/AppliedPY/Final project folder/GlobalLandTemperaturesByMajorCity.csv"
+df = pd.read_csv(pathname)
 
 cities = set(df.City)
 
